0e6b8654c842cac583d0d2d7b548a5e6008d4da3
Sri Ram Siva S <sriramsivasriramsiva1@gmail.com>
2026-05-25 17:28:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,68 +1,37 @@
 const express = require("express");
+
 const dotenv = require("dotenv");
+
 const cors = require("cors");
-const helmet = require("helmet");
 
 const connectDB = require("./config/db");
 
 dotenv.config();
 
+connectDB();
+
 const authRoutes = require("./routes/authRoutes");
+
 const blogRoutes = require("./routes/blogRoutes");
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
-
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  "http://localhost:5173",
-];
-
-app.use(helmet());
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
+    origin: "https://blog-rho-ruby-31.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 
-app.options("*", cors());
-
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API Running...");
-});
-
 app.use("/api/auth", authRoutes);
+
 app.use("/api/blogs", blogRoutes);
 
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    success: false,
-    message: err.message,
-  });
+app.listen(process.env.PORT, () => {
+
+    console.log("Server Running");
+
 });
-
-const startServer = async () => {
-  try {
-    await connectDB();
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.log("Database connection failed:", error);
-  }
-};
-
-startServer();
