64a8fd4c21cd4542cffc079779405d7e315bc52a
Sri Ram Siva S <sriramsivasriramsiva1@gmail.com>
2026-05-25 15:43:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,8 +29,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.use("/api/blogs", blogRoutes);
-const express = require('express')
-const app = express()
+
 const port = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
