6323bfc91c468b75240c54b39a16a90cbc02ebc2
Sri Ram Siva S <sriramsivasriramsiva1@gmail.com>
2026-05-25 14:50:41 +0530

    Update CORS origin to include environment variable

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:[process.env.FRONTEND_URL, "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
