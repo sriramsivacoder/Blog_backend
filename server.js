a64047a47a7a1973331af0a2b6ada6c63e0abedb
Sri Ram Siva S <sriramsivasriramsiva1@gmail.com>
2026-06-11 22:16:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,13 +17,7 @@ const blogRoutes = require("./routes/blogRoutes");
 const app = express();
 
 app.use(
-  cors({
-    origin: "https://blog-rho-ruby-31.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-app.options("/*", cors());
+  cors({});
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
