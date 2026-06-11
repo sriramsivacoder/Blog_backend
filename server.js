6404220353391fbceb0bd69acc010244bd3a09d7
Sri Ram Siva S <sriramsivasriramsiva1@gmail.com>
2026-06-11 22:28:25 +0530

    Refactor server.js for improved readability

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
-
 const dotenv = require("dotenv");
-
 const cors = require("cors");
 
 const connectDB = require("./config/db");
@@ -11,20 +9,18 @@ dotenv.config();
 connectDB();
 
 const authRoutes = require("./routes/authRoutes");
-
 const blogRoutes = require("./routes/blogRoutes");
 
 const app = express();
 
-app.use(cors({});
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-
 app.use("/api/blogs", blogRoutes);
 
-app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 10000;
 
-    console.log("Server Running");
-
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server Running on ${PORT}`);
 });
